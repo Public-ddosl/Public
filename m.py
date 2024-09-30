d4bd2d344a1817bef09e3a522e644e0923c4c0c8
Public-ddosl <dcdc81122@gmail.com>
2024-09-30 07:20:46 +0530

    Update m.py

--- m.py
+++ m.py
@@ -8,10 +8,10 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7543594921:AAEb0Qs9usGggKGl0Q-iZ-JimDzJhtv01e0')
+bot = telebot.TeleBot('7251424139:AAGT3KphpsFTr0sWKfVK2pn3xJpiEHgViFI')
 
 # Admin user IDs
-admin_id = ["5894556386"]
+admin_id = ["7182006681"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
